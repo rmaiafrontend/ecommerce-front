565c09f26689f6fd94e61eebbbc130cc958a97aa
rmaiafrontend <rmaiafrontend@gmail.com>
2026-04-09 19:14:32 -0300

    ajuste .env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ecommerce-back-5jun.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5173',
 });
 
 api.interceptors.request.use((config) => {
